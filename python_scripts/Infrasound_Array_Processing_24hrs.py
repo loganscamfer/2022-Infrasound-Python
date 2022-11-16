011edac6282fc0c105a7f71c98e9aeb4353eda26
loganscamfer <102924789+loganscamfer@users.noreply.github.com>
2022-11-16 16:26:22 -0700

    Added error messages to array_processing loop

--- python_scripts/Infrasound_Array_Processing_24hrs.py
+++ python_scripts/Infrasound_Array_Processing_24hrs.py
@@ -54,15 +54,19 @@ for start_time_morning in start_times:
         end_time = start_time_morning + 86400
         st = client.get_waveforms("XP","PARK","??","HDF",start_time_morning,end_time)
         add_inv_coords(st, inv)
-    
-        Array_1 = array_processing(st,10,0.5,sll_x = -4, slm_x = 4, sll_y = -4, slm_y = 4, 
-                 sl_s = 0.1, semb_thres = 0, vel_thres = 0, frqlow= 5,
-                 frqhigh=20, stime = start_time_morning, etime = end_time,
-                 prewhiten = False, verbose = False, coordsys = 'lonlat',
-                 timestamp = 'mlabday', method = 0, store = None)
-    
-        filename = start_time_morning.strftime('%Y%m%d_%H%M') +'all_day.pkl'
-        pickle.dump(Array_1,open('pickle_files/'+filename,'wb'))
+        try:
+            Array_1 = array_processing(st,10,0.5,sll_x = -4, slm_x = 4, sll_y = -4, slm_y = 4, 
+                    sl_s = 0.1, semb_thres = 0, vel_thres = 0, frqlow= 5,
+                    frqhigh=20, stime = start_time_morning, etime = end_time,
+                    prewhiten = False, verbose = False, coordsys = 'lonlat',
+                    timestamp = 'mlabday', method = 0, store = None)
+            filename = start_time_morning.strftime('%Y%m%d_%H%M') +'all_day.pkl'
+            pickle.dump(Array_1,open('pickle_files/'+filename,'wb')
+        except:
+            failure_date1 = start_time_morning.strftime('%Y%m%d_%H%M')
+            print('Array Processing failed at  ' + failure_date1)
     except:
-        pass
+            failure_date = start_time_morning.strftime('%Y%m%d_%H%M')
+            print('Get waveforms failed on ' + failure_date)
+            pass
 
